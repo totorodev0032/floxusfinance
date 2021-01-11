268a6fcf5441c1708c591a84345fdbe845b487ca
nagendra0032 <nagendrakumar0032@gmail.com>
2021-01-11 23:14:37 +0530

    typo corrected

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import { Link } from 'gatsby';
 
 export const query = graphql`

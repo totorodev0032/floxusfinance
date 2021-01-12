6ff018f85a921eeca28dc0a91d5f3e5fc4aeaed0
nagendra0032 <nagendrakumar0032@gmail.com>
2021-01-12 20:26:25 +0530

    subscribe section improved

--- src/components/SubscribeContainer/SubscribeContainer.js
+++ src/components/SubscribeContainer/SubscribeContainer.js
@@ -4,19 +4,36 @@ import SubscribeInput from '../SubscribeInput/SubscribeInput';
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
-  height: 400px;
+  height: 250px;
   align-items: center;
   justify-content: center;
   background-color: white;
+`;
+
+const Heading = styled.p`
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color: black;
+  ${'' /* color:#2A2552; */}
+  font-weight: 700;
+  font-size: 30px;
+  text-align: center;
+  @media (max-width: 496px) {
+    font-size: 20px;
+  }
 `;
 
 const SubscribeContainer = () => {
   return (
     <>
       <Wrapper>
+        <Heading>Subscribe to our blog.</Heading>
         <SubscribeInput />
+        <Heading>Logo</Heading>
       </Wrapper>
+      {/* <p>by Floxus </p> */}
     </>
   );
 };
